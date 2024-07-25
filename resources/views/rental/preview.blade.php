bea9b16b085dc072453e24b231172b879eff9d85
reysilvaa <reynaldsilva123@gmail.com>
2024-07-25 22:58:41 +0700

    fix preview invoice

--- resources/views/rental/preview.blade.php
+++ resources/views/rental/preview.blade.php
@@ -1,11 +1,14 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Invoice Preview</title>
-</head>
-<body>
-    <h1>Invoice Preview</h1>
-    <iframe src="data:application/pdf;base64,{{ base64_encode($pdf) }}" style="width: 100%; height: 600px;" frameborder="0"></iframe>
-    <a href="{{ route('transaksi.invoice.download', $transaksi->id) }}" class="btn btn-primary">Download PDF</a>
-</body>
-</html>
+@extends('layouts.admin')
+
+@section('title', 'Invoice Preview')
+
+@section('content')
+    <div class="container mx-auto px-4 py-6">
+        <div class="bg-white shadow-md rounded-lg p-4 z-10 relative">
+            <iframe src="data:application/pdf;base64,{{ base64_encode($pdf) }}" style="width: 100%; height: 600px;" frameborder="0"></iframe>
+        </div>
+        <div class="mt-4 z-10 relative">
+            <a href="{{ route('transaksi.invoice.download', $transaksi->id) }}" class="btn btn-primary">Download PDF</a>
+        </div>
+    </div>
+@endsection
