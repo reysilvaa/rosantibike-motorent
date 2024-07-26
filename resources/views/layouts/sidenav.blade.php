5034bf1c3890ffbd64b8bafbbc90e39566186ed8
reysilvaa <reynaldsilva123@gmail.com>
2024-07-27 00:35:41 +0700

    add logout button in sidenavbar

--- resources/views/layouts/sidenav.blade.php
+++ resources/views/layouts/sidenav.blade.php
@@ -1,12 +1,16 @@
 <div id="layoutSidenav_nav" class="w-64 bg-gray-200 text-gray-600 shadow-lg">
     <nav class="flex flex-col h-full">
         <!-- Sidebar Header -->
-        <div class="flex-1 px-4 py-6">
+        <div class="flex-1 px-4 py-6 mb-10">
             <div class="text-xs font-semibold uppercase tracking-wider mb-6 text-gray-500">Core</div>
-            <a class="flex items-center px-4 py-2 text-gray-600 hover:bg-gray-300 hover:text-gray-800 rounded-md transition duration-300 ease-in-out no-underline" href="{{ route('dashboard') }}">
-                <i class="fas fa-tachometer-alt mr-3"></i>
-                Dashboard
-            </a>
+                <a class="flex items-center px-4 py-2 text-gray-600 hover:bg-gray-300 hover:text-gray-800 rounded-md transition duration-300 ease-in-out no-underline mb-4" href="{{ route('dashboard') }}">
+                    <i class="fas fa-tachometer-alt mr-3"></i>
+                    Dashboard
+                </a>
+                <a class="flex items-center px-4 py-2 bg-red-600 text-white hover:bg-red-700 rounded-md transition duration-300 ease-in-out no-underline" href="{{ route('logout') }}">
+                    <i class="fas fa-sign-out-alt mr-3"></i>
+                    Keluar
+                </a>
             <div class="text-xs font-semibold uppercase tracking-wider mt-6 mb-4 px-4 text-gray-500">Interface</div>
             <a class="flex items-center px-4 py-2 text-gray-600 hover:bg-gray-300 hover:text-gray-800 rounded-md transition duration-300 ease-in-out no-underline" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
                 <i class="fas fa-columns mr-3"></i>
