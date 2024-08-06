da72f566d4290333fe0267cedcf8a52767b94e67
reysilvaa <reynaldsilva123@gmail.com>
2024-08-06 22:24:04 +0700

    duplicate cdn alpine.js removed

--- resources/views/admin/landing/galeri/edit.blade.php
+++ resources/views/admin/landing/galeri/edit.blade.php
@@ -60,5 +60,4 @@
     </form>
 </div>
 
-{{-- <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" defer></script> --}}
 @endsection
--- resources/views/layouts/styles.blade.php
+++ resources/views/layouts/styles.blade.php
@@ -10,6 +10,7 @@
     <link rel="preload" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" as="style">
     <link rel="preload" href="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" as="script">
 
+
     <!-- Include Google Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -22,9 +23,6 @@
 </head>
 <body>
     <!-- Your content here -->
-
-    <!-- Include AlpineJS -->
-    <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
 
     <!-- Include FontAwesome (JS) -->
     <script src="https://use.fontawesome.com/releases/v6.4.0/js/all.js" crossorigin="anonymous" defer></script>
