7d9a639e38faa4ef133f5acccf2f7e4ccd8e7d50
reysilvaa <reynaldsilva123@gmail.com>
2024-08-10 23:15:47 +0700

    changes of direction to location in landing page

--- resources/views/landing/assets/promo.blade.php
+++ resources/views/landing/assets/promo.blade.php
@@ -5,8 +5,7 @@
                 <h2 class="text-3xl font-bold mb-4">Temukan Lokasi Kami!</h2>
                 <p class="text-xl mb-6">Klik tombol di bawah untuk mendapatkan petunjuk arah ke <span class="font-bold">ROSANTIBIKE MOTORENT</span>. Kami siap membantu Anda!</p>
                 <a
-                href="https://www.google.com/maps/dir/?api=1&destination=ROSANTIBIKE%20MOTORENT%2C+Jl.+Sukorame+Utara%2C+Jl.+Sukorame+Baru%2C+Kec.+Lowokwaru%2C+Kota+Malang%2C+Jawa+Timur+65141%2C+Indonesia&travelmode=driving"
-                target="_blank"
+                href="https://maps.app.goo.gl/QGVYs7wVaMxEUtmG6"
                 class="bg-white text-blue-600 py-2 px-6 rounded-full text-lg font-semibold hover:bg-blue-100 transition duration-300 ease-in-out inline-block w-full md:w-auto"
             >
                 Dapatkan Petunjuk Arah
@@ -15,7 +14,7 @@
             <div class="md:w-1/2">
                 <iframe
                 class="rounded-lg shadow-lg w-full h-full aspect-ratio-1/1 md:aspect-ratio-16/9"
-                src="https://maps.app.goo.gl/9RRBNw6brUwKR68R9"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3951.4114755690393!2d112.64555067455653!3d-7.9563592792792965!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd62959ae7e7a37%3A0xa90ec3b14d2aa16c!2sROSANTIBIKE%20MOTORENT!5e0!3m2!1sid!2sid!4v1722079814753!5m2!1sid!2sid"
                 style="border:0; width: 100%; height: 300px;"
                 allowfullscreen=""
                 loading="lazy"
