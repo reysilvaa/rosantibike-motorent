6130bc74a0a189552345fa8314a333d9007b2cdf
reysilvaa <reynaldsilva123@gmail.com>
2025-01-24 20:27:09 +0700

    test2

--- routes/api.php
+++ routes/api.php
@@ -32,8 +32,22 @@ Route::prefix('admin/transaksi')->middleware('auth:api')->group(function () {
     Route::get('/datatable', [AdminTransaksiController::class, 'getData']);
 });
 
-Route::apiResource('admin/jenis-motor', JenisMotorController::class)->middleware('auth:api');
-
+Route::prefix('admin/jenis-motors')->group(function () {
+    // Store (Create) a new JenisMotor
+    Route::post('/', [JenisMotorController::class, 'store']);
+    
+    // Update an existing JenisMotor by ID
+    Route::put('/{id}', [JenisMotorController::class, 'update']);
+    
+    // List all JenisMotor
+    Route::get('/', [JenisMotorController::class, 'index']);
+    
+    // Show a specific JenisMotor by ID
+    Route::get('/{id}', [JenisMotorController::class, 'show']);
+    
+    // Delete a specific JenisMotor by ID
+    Route::delete('/{id}', [JenisMotorController::class, 'destroy']);
+});
 Route::prefix('transaksi')->group(function () {
     Route::get('/', [TransaksiController::class, 'index']);
     Route::post('/create', [TransaksiController::class, 'create']);
