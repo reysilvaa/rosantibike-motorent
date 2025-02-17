af9878efc1f183f82c6ad2a4e59ae3bebc980a4d
reysilvaa <reynaldsilva123@gmail.com>
2025-02-17 18:25:58 +0700

    test CI/CD

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <!-- Google tag (gtag.js) -->
+    <!-- Google tag Manager(gtag.js) -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-F5L46F7N9X"></script>
     <script>
     window.dataLayer = window.dataLayer || [];
